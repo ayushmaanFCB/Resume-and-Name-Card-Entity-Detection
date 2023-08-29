ff058476a05a49275027165b7de5ca3f2d2e7618
Ayushmaan Das <dasayush5maan@gmail.com>
2023-08-29 21:53:53 +0530

    docanno 30 resumes

--- main.py
+++ main.py
@@ -1,43 +1,55 @@
 import gradio
 import spacy
 from spacy import displacy
+import json
 
-# resume_nlp = spacy.load("./ner_resumes/model-best")
+from scripts.text_from_pdf import extractFromPDF
+
+try:
+    resume_nlp = spacy.load("./model_Docanno30")
+    print("\nModel Loaded Successfully.......... Launching Gradio App")
+except Exception as e:
+    print(e)
 
 
-def resume_ner(text):
-    # doc = resume_nlp(text)
-    doc = ""
+def resume_ner(filePath):
+    text = extractFromPDF(filePath)
+    doc = resume_nlp(text)
 
-    color_schemes = {
-        "College Name": "red",
-        "Companies worked at": "pink",
-        "Degree": "yellow",
-        "Designation": "orange",
-        "Email Address": "cyan",
-        "Graduation Year": "green",
-        "Location": "magenta",
-        "Name": "blue",
-        "Skills": "green",
-        "Years of Experience": "purple"
-    }
+    with open("./assets/label_config.json", "r") as file:
+        datas = json.load(file)
+    color_schemes = {}
+    for data in datas:
+        color_schemes.update({data['text']: data['backgroundColor']})
 
     output = displacy.render(doc, style="ent", page=True, options={
                              "colors": color_schemes})
-    html = "<div style='max-width:100%; overflow:auto'>" + output + "</div>"
+
+    html = "<h1 style='text-align:center'>Detected Entities</h1><div style='max-width:100%; overflow:auto'>" + \
+        output + "</div><hr>"
 
     return html
 
 
-def file_process(file):
-    print(file)
+with gradio.Blocks() as block:
+    with gradio.Row():
+        gradio.HTML(
+            "<h1 style='text-align:center; font-size:40px'>Entity Recognition from Resume using Custom Trained Model</h1><hr>")
+    with gradio.Row():
+        with gradio.Column():
+            upload_button = gradio.UploadButton(
+                "Click to Upload a Resume File", file_count="single")
+    with gradio.Row():
+        upload_button.upload(resume_ner, upload_button, gradio.HTML(
+        ), show_progress=True, scroll_to_output=True)
 
 
-iface = gradio.Interface(
-    fn=file_process,
-    inputs=gradio.inputs.File(label="Upload Resume Document"),
-    outputs=gradio.HTML(label="Detected Entities")
-)
+block.launch()
 
 
-iface.launch()
+# iface = gradio.Interface(
+#     fn=resume_ner,
+#     inputs=gradio.File(label="Upload Resume Document"),
+#     outputs=gradio.HTML(label="Detected Entities")
+# )
+# iface.launch()
---
+++ scripts/text_from_pdf.py
@@ -0,0 +1,20 @@
+import re
+import PyPDF2
+
+
+def extractFromPDF(filePath):
+
+    filePath = filePath.name
+
+    if filePath.endswith(".pdf"):
+        pdf_reader = PyPDF2.PdfReader(filePath)
+        text = ''
+        for page in range(len(pdf_reader.pages)):
+            page_obj = pdf_reader.pages[page]
+            text += escape_ansi(page_obj.extract_text())
+        return text
+
+
+def escape_ansi(line):
+    ansi_escape = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
+    return ansi_escape.sub('', line).replace("\n", ".").replace("*", "").replace("\\", "")
