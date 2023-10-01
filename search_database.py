d425985e39de5421002510cadb7122940fc96c84
Ayushmaan Das <dasayush5maan@gmail.com>
2023-10-01 13:25:31 +0530

    db

--- search_database.py
+++ search_database.py
@@ -3,16 +3,23 @@ import re
 from mongodb_connect import searchFromDB
 
 
-def search(parameter, keyword):
+def searchResumes(parameter, keyword):
     results = searchFromDB(parameter, keyword)
     flag = False
     output = """"""
     for result in results:
-        if result['Key Points']['basics']['name'] != None:
-            flag = True
-            
-    if flag == False:
-        output = """# Sorry, No such record exists in the database"""
+        name = result['Key Points']['basics']['name'][0]
+        phone = result['Key Points']['basics']['phone']
+        email = result['Key Points']['basics']['email']
+        skills = result['Key Points']['expertise']['skill']
+        output = output + \
+            "<h1>{0}</h1><h3>PHONE : {1}</h3><h3>EMAIL : {2}</h3><h3>SKILLS : </h3>".format(
+                name, ", ".join(map(str, phone)),  ", ".join(map(str, email)))
+        for skill in skills:
+            output = output + "\t".format(skill)
+
+    # if flag == False:
+    #     output = """<h1> Sorry, No such record exists in the database </h1>"""
     return output
 
 
@@ -21,5 +28,5 @@ with gradio.Blocks() as block:
         parameter = gradio.Radio(choices=["NAME", "ID", "EMAIL", "PHONE"])
         keyword = gradio.Text(label="Enter the value")
         submit = gradio.Button(value="SEARCH")
-        submit.click(fn=search, inputs=[
-                     parameter, keyword], outputs=[gradio.Markdown()])
+        submit.click(fn=searchResumes, inputs=[
+                     parameter, keyword], outputs=[gradio.HTML()])
