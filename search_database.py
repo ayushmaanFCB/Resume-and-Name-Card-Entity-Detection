20d8db5bbc820bb8178005608fed981f6ed16ef3
Ayushmaan Das <dasayush5maan@gmail.com>
2023-10-03 16:29:55 +0530

    menu-frontend

--- search_database.py
+++ search_database.py
@@ -5,59 +5,61 @@ from mongodb_connect import searchFromDB
 
 def searchResumes(parameter, keyword):
     flag = False
-    results = searchFromDB(parameter, keyword)
-    output = """<div>"""
-    for result in results:
-        flag = True
-        id = result["_id"]
-        try:
-            name = result['Key Points']['basics']['name'][0]
-        except:
-            name = "N.A."
-        phone = result['Key Points']['basics']['phone']
-        email = result['Key Points']['basics']['email']
-        languages = result['Key Points']['basics']['language']
-        locations = result['Key Points']['basics']['location']
-        urls = result['Key Points']['basics']['url']
-        skills = result['Key Points']['expertise']['skill']
-        companies = result['Key Points']['work']['company']
-        positions = result['Key Points']['work']['position']
-        experiences = result['Key Points']['work']['experience']
-        roles = result['Key Points']['work']['role']
 
-        phone, email, languages, locations = [
-            "-" if not data else data for data in (phone, email, languages, locations)]
-        output = output + \
-            """<h1 style='text-align:center'>{0}</h1> <table style='margin-right:auto; margin-left:auto'><tr><th>ID</th><th>CONTACT NUMBER</th><th>EMAIL IDs</th><th>LOCATIONS</th><th>LANGUAGES</th></tr>""".format(
-                name.upper())
-        output = output + """<tr><tr><td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td><td>{4}</td></tr>""".format(
-            id, ", ".join(map(str, phone)), ", ".join(map(str, email)), ", ".join(
-                map(str, locations)), ", ".join(map(str, languages))
-        )
-        output = output + "</table>"
+    if len(keyword) > 3:
+        results = searchFromDB(parameter, keyword)
+        output = """<div>"""
+        for result in results:
+            flag = True
+            id = result["_id"]
+            try:
+                name = result['Key Points']['basics']['name'][0]
+            except:
+                name = "N.A."
+            phone = result['Key Points']['basics']['phone']
+            email = result['Key Points']['basics']['email']
+            languages = result['Key Points']['basics']['language']
+            locations = result['Key Points']['basics']['location']
+            urls = result['Key Points']['basics']['url']
+            skills = result['Key Points']['expertise']['skill']
+            companies = result['Key Points']['work']['company']
+            positions = result['Key Points']['work']['position']
+            experiences = result['Key Points']['work']['experience']
+            roles = result['Key Points']['work']['role']
 
-        datas = [companies, positions, skills, urls]
-        messages = ["<h2>Companies worked at : </h2>", "<h2>Job Designations / Positions : </h2>",
-                    "<h2>Applicant Skillset : </h2>", "<h2>Profile reference links : </h2>"]
-        for i, data in enumerate(datas):
-            if data != []:
-                output = output + "<h2>{0}</h2>".format(messages[i])
-                for info in data:
-                    output = output + \
-                        "<span style='margin-right: 30px'>{0}</span>".format(
-                            info)
+            phone, email, languages, locations = [
+                "-" if not data else data for data in (phone, email, languages, locations)]
+            output = output + \
+                """<h1 style='text-align:center'>{0}</h1> <table style='margin-right:auto; margin-left:auto'><tr><th>ID</th><th>CONTACT NUMBER</th><th>EMAIL IDs</th><th>LOCATIONS</th><th>LANGUAGES</th></tr>""".format(
+                    name.upper())
+            output = output + """<tr><tr><td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td><td>{4}</td></tr>""".format(
+                id, ", ".join(map(str, phone)), ", ".join(map(str, email)), ", ".join(
+                    map(str, locations)), ", ".join(map(str, languages))
+            )
+            output = output + "</table>"
 
-        datas = [experiences, roles]
-        messages = ["<h2>Work Experiences and Expertise : </h2>",
-                    "<h2>Roles and Tasks performed : </h2>"]
-        for i, data in enumerate(datas):
-            if data != []:
-                output = output + "<h2>{0}</h2><ul>".format(messages[i])
-                for info in data:
-                    output = output + \
-                        "<li>{0}</li>".format(
-                            info)
-                output = output + "</ul>"
+            datas = [companies, positions, skills, urls]
+            messages = ["<h2>Companies worked at : </h2>", "<h2>Job Designations / Positions : </h2>",
+                        "<h2>Applicant Skillset : </h2>", "<h2>Profile reference links : </h2>"]
+            for i, data in enumerate(datas):
+                if data != []:
+                    output = output + "<h2>{0}</h2>".format(messages[i])
+                    for info in data:
+                        output = output + \
+                            "<span style='margin-right: 30px'>{0}</span>".format(
+                                info)
+
+            datas = [experiences, roles]
+            messages = ["<h2>Work Experiences and Expertise : </h2>",
+                        "<h2>Roles and Tasks performed : </h2>"]
+            for i, data in enumerate(datas):
+                if data != []:
+                    output = output + "<h2>{0}</h2><ul>".format(messages[i])
+                    for info in data:
+                        output = output + \
+                            "<li>{0}</li>".format(
+                                info)
+                    output = output + "</ul>"
 
     if flag == False:
         output = """
@@ -100,10 +102,10 @@ def searchByParameters(parameter, keyword):
 
     if flag == False:
         output = """
-        <p style='text-align:center'>
-        <img width="96" height="96" src="https://img.icons8.com/color-glass/96/user-not-found.png" alt="user-not-found"/>
-        <h1 style='color:red'>Sorry, No match has been found. Check the details entered.</h1>
-        </p>
+            <div style='text-align:center; margin: 0 auto;'>
+            <img style='margin: 0 auto;' width="96" height="96" src="https://img.icons8.com/color-glass/96/user-not-found.png" alt="user-not-found"/>
+            <h1 style='color:red'>Sorry, No such record exists. Check the details entered.</h1>
+            </div>
         """
     else:
         output = "<h1 style='text-align:center'>RECORDS FETCHED</h1>" + output
@@ -112,7 +114,8 @@ def searchByParameters(parameter, keyword):
 
 with gradio.Blocks() as block:
     with gradio.Row():
-        gradio.HTML("<h1 style='text-align:center'>DATABASE QUERYING<h1>")
+        gradio.HTML(
+            "<h1 style='text-align:center; font-size:40px'>DATABASE QUERYING - Empowering Insights and Navigating Data<h1>")
 
     with gradio.Tab(label="SEARCH FOR UPLOADED RESUMES") as tab1:
         with gradio.Row():
@@ -130,7 +133,7 @@ with gradio.Blocks() as block:
         with gradio.Row():
             with gradio.Column(scale=4):
                 parameter = gradio.Dropdown(label="WHAT DO YOU WANT TO SEARCH USING ?",
-                                            choices=["SKILL", "LOCATION", "CERTIFICATION"])
+                                            choices=["SKILL", "COURSE", "POSITION", "EXPERIENCE", "COMPANY", "LOCATION", "CERTIFICATION"])
                 keyword = gradio.Text(label="ENTER THE SEARCH DATA")
             with gradio.Row():
                 submit = gradio.Button(value="SEARCH THE DATABASE")
